b8d8b00df5331c9fcd3be4297df2a2487bd85d41
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2020-11-10 21:02:47 +0000

    shows the block level in the statusbar (and some cleanups)

--- src/bftextview2.c
+++ src/bftextview2.c
@@ -495,7 +495,7 @@ bluefish_text_view_get_active_identifier(BluefishTextView * btv, GtkTextIter * c
 	return (patnum != FALSE);
 }
 
-static gchar *blockstack_string(BluefishTextView * btv, Tfoundblock * fblock)
+static gchar *blockstack_string(BluefishTextView * btv, Tfoundblock * fblock, gint *level)
 {
 	GString *tmp;
 	Tfoundblock *parent;
@@ -505,6 +505,7 @@ static gchar *blockstack_string(BluefishTextView * btv, Tfoundblock * fblock)
 	parent = fblock;
 	tmp = g_string_new("");
 	while (parent) {
+		(*level)++;
 		if (parent->start2_o != BF_OFFSET_UNDEFINED) {
 			if (parent != fblock)
 				tmp = g_string_prepend(tmp, " ");
@@ -530,6 +531,15 @@ static gchar *blockstack_string(BluefishTextView * btv, Tfoundblock * fblock)
 	return g_string_free(tmp, FALSE);
 }
 
+gboolean foundblock_is_at_boundary_location(Tfoundblock *fblock, guint offset) {
+	if (fblock->start2_o != BF_OFFSET_UNDEFINED
+			&& (fblock->start1_o == offset || fblock->end1_o == offset || fblock->start2_o == offset
+				|| fblock->end2_o == offset)) {
+		return TRUE;
+	}
+	return FALSE;
+}
+
 /* because we don't want to expose the Tfoundblock to the external API we return gpointer
 external functions should treat this as a boolean: NULL means there is no block, a pointer means there is a block
 */
@@ -542,13 +552,9 @@ bftextview2_get_block_at_boundary_location(BluefishTextView * btv, guint offset,
 	fblock = bftextview2_get_active_block_at_offset(btv->master, FALSE, offset);
 	if (fblock)
 		fblock = first_fully_defined_block(fblock);
-	if (fblock) {
-		if (fblock->start2_o != BF_OFFSET_UNDEFINED
-			&& (fblock->start1_o == offset || fblock->end1_o == offset || fblock->start2_o == offset
-				|| fblock->end2_o == offset)) {
-			bftextview2_get_iters_at_foundblock(btv->buffer, fblock, it1, it2, it3, it4);
-			return fblock;
-		}
+	if (fblock && foundblock_is_at_boundary_location(fblock, offset)) {
+		bftextview2_get_iters_at_foundblock(btv->buffer, fblock, it1, it2, it3, it4);
+		return fblock;
 	}
 	return NULL;
 }
@@ -559,6 +565,7 @@ static gboolean mark_set_idle_lcb(gpointer widget)
 	BluefishTextView *master = btv->master;
 	GtkTextIter it1, it2, it3, it4, location;
 	Tfoundblock *fblock;
+	gint level=0;
 	gchar *tmpstr = NULL;
 	guint offset;
 
@@ -570,48 +577,24 @@ static gboolean mark_set_idle_lcb(gpointer widget)
 	}
 
 	offset = gtk_text_iter_get_offset(&location);
-	fblock = (Tfoundblock *) bftextview2_get_block_at_boundary_location(btv, offset, &it1, &it2, &it3, &it4);
-	/*fblock = bftextview2_get_active_block_at_offset(master, FALSE, offset);
-	   DBG_BLOCKMATCH("mark_set_idle_lcb, got fblock %p\n", fblock);
-	   DBG_SIGNALS("mark_set_idle_lcb, 'insert' set at %d\n", gtk_text_iter_get_offset(&location));
-	   if (fblock)
-	   fblock = first_fully_defined_block(fblock);
-	   if (fblock) {
-	   if (fblock->start1_o == offset || fblock->end1_o == offset || fblock->start2_o == offset
-	   || fblock->end2_o == offset) {
-	   GtkTextIter it3, it4;
-
-	   DBG_BLOCKMATCH("mark_set_idle_lcb, got fblock %p with offsets %d:%d %d:%d\n", fblock,
-	   fblock->start1_o, fblock->end1_o, fblock->start2_o, fblock->end2_o);
-	   if (fblock->start2_o != BF_OFFSET_UNDEFINED) {
-	   bftextview2_get_iters_at_foundblock(btv->buffer, fblock, &it1, &it2, &it3, &it4);
-	   DBG_MSG("mark_set_idle_lcb, found a block to highlight the start (%d:%d) and end (%d:%d)\n",
-	   gtk_text_iter_get_offset(&it1), gtk_text_iter_get_offset(&it2),
-	   gtk_text_iter_get_offset(&it3), gtk_text_iter_get_offset(&it4));
-	   gtk_text_buffer_apply_tag(btv->buffer, master->blockmatch, &it1, &it2);
-	   gtk_text_buffer_apply_tag(btv->buffer, master->blockmatch, &it3, &it4);
-	   btv->showing_blockmatch = TRUE;
-	   } else {
-	   DBG_MSG("mark_set_idle_lcb, block has no end - no matching\n");
-	   }
-	   } */
+	fblock = bftextview2_get_active_block_at_offset(btv->master, FALSE, offset);
+	if (fblock)
+		fblock = first_fully_defined_block(fblock);
 	if (fblock) {
-		gtk_text_buffer_apply_tag(btv->buffer, master->blockmatch, &it1, &it2);
-		gtk_text_buffer_apply_tag(btv->buffer, master->blockmatch, &it3, &it4);
-		btv->showing_blockmatch = TRUE;
-		if (BFWIN(DOCUMENT(master->doc)->bfwin)->session->view_blockstack)
-			tmpstr = blockstack_string(btv, fblock);
-	}							/*else if (found && found->fblock && BFWIN(DOCUMENT(btv->doc)->bfwin)->session->view_blockstack) {
-								   fblock = found->fblock;
-								   if (found->numblockchange < 0) {
-								   fblock = pop_blocks(found->numblockchange, fblock);
-								   }
-								   tmpstr = blockstack_string(btv, fblock);
-								   } */
-	if (tmpstr) {
-		bfwin_statusbar_message(DOCUMENT(master->doc)->bfwin, tmpstr, 2);
-		g_free(tmpstr);
+		if (foundblock_is_at_boundary_location(fblock, offset)) {
+			bftextview2_get_iters_at_foundblock(btv->buffer, fblock, &it1, &it2, &it3, &it4);
+			gtk_text_buffer_apply_tag(btv->buffer, master->blockmatch, &it1, &it2);
+			gtk_text_buffer_apply_tag(btv->buffer, master->blockmatch, &it3, &it4);
+			btv->showing_blockmatch = TRUE;
+		}
+		tmpstr = blockstack_string(btv, fblock, &level);
 	}
+
+	if (BFWIN(DOCUMENT(master->doc)->bfwin)->session->view_blockstack && tmpstr) {
+		bfwin_statusbar_message(DOCUMENT(master->doc)->bfwin, tmpstr, 2);
+	}
+	g_free(tmpstr);
+	doc_set_statusbar_lncol(DOCUMENT(master->doc), level);
 	btv->mark_set_idle = 0;
 	return FALSE;
 }
